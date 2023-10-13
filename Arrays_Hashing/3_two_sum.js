e688ba0a3c27f3d324d6bede0bc3ffa5b54fcd1b
Saurabh Ramesh Kacholiya <kacholiyasaurabh@gmail.com>
2023-10-13 23:59:01 +0530

    feat: two sum problem

---
+++ Arrays_Hashing/3_two_sum.js
@@ -0,0 +1,35 @@
+// LeetCode https://leetcode.com/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// TC -> O(N^2)
+// SC -> O(1)
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (target === nums[i] + nums[j]) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// TC -> O(N)
+// SC -> O(N)
+var twoSum = function (nums, target) {
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    obj[nums[i]] = i;
+  }
+  for (key in obj) {
+    const missingNumber = target - Number(k);
+    console.log("missingNumber", missingNumber);
+    if (obj[missingNumber]) {
+      return [obj[k], obj[missingNumber]];
+    }
+  }
+};
