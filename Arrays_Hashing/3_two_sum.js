87bb9aececf8fd053bae49b245d7526f08f25c8d
Saurabh Ramesh Kacholiya <kacholiyasaurabh@gmail.com>
2023-10-15 21:58:29 +0530

    feat: update

--- Arrays_Hashing/3_two_sum.js
+++ Arrays_Hashing/3_two_sum.js
@@ -8,15 +8,15 @@
 
 // TC -> O(N^2)
 // SC -> O(1)
-// var twoSum = function (nums, target) {
-//   for (let i = 0; i < nums.length; i++) {
-//     for (let j = i + 1; j < nums.length; j++) {
-//       if (target === nums[i] + nums[j]) {
-//         return [i, j];
-//       }
-//     }
-//   }
-// };
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (target === nums[i] + nums[j]) {
+        return [i, j];
+      }
+    }
+  }
+};
 
 // TC -> O(N)
 // SC -> O(N)
@@ -30,7 +30,3 @@ var twoSum = function (nums, target) {
     obj[nums[i]] = i;
   }
 };
-
-const v = twoSum([3, 3], 6);
-
-console.log("value is ", v);
